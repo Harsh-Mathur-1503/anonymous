05e5a5639df24e9e99cdeaabf91beadf6e71e7b2
Harsh Mathur <harsh@Harshs-MacBook-Air.local>
2024-07-27 17:35:56 +0530

    database conection part done

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+    isConnected?: number;
+}
+
+const connection:ConnectionObject = {}
+
+async function dbConnect():Promise<void> {
+    if(connection.isConnected) {
+        console.log("Database is already connected");
+        return;
+    }
+
+    try {
+        const db = await mongoose.connect(process.env.MONGODB_URI || "", {});
+        console.log("Database connected - ",db);
+
+        connection.isConnected = db.connections[0].readyState;
+        console.log("Connection state - ", connection.isConnected);
+    } catch (error) {
+        console.error("Error connecting to database - ", error);
+        process.exit(1);
+    }
+}
